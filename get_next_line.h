00eef65dcea07aa6e3261364f358a76414312761
luda-cun <lucien.dacunha@gmal.com>
2025-01-22 15:40:11 +0100

    version 0.6 | norminette OK

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: luda-cun <luda-cun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 18:49:22 by luda-cun          #+#    #+#             */
-/*   Updated: 2025/01/22 15:21:55 by luda-cun         ###   ########.fr       */
+/*   Updated: 2025/01/22 15:39:42 by luda-cun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# ifndef BUFFER_SIZE 
+# ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
 # endif
 
@@ -28,6 +28,5 @@ char	*get_next_line(int fd);
 char	*ft_strjoin(const char *s1, const char *s2);
 int		ft_strlen(const char *str);
 void	*ft_calloc(size_t count, size_t size);
-
 
 #endif
